89178394dd0df38797088ae86b721ee88952042d
Milena Stancheva <mil.stancheva@gmail.com>
2017-05-12 14:16:19 +0300

    Problem 12 - Queens Puzzle

---
+++ Data Structures&Аlgorithms/Recursion Homework/12.QueensPuzzle/QueenPuzzleSolver.cs
@@ -0,0 +1,76 @@
+﻿namespace _12.QueensPuzzle
+{
+    public class QueenPuzzleSolver
+    {
+        private byte[,] queens;
+        private int count = 0;
+
+        public QueenPuzzleSolver(int size)
+        {
+            this.queens = new byte[size, size];
+        }
+
+        public int FindAllSolutions()
+        {
+            Recursions(0);
+            return this.count;
+        }
+
+        private void Recursions(int row)
+        {
+            if (row == this.queens.GetLength(1))
+            {
+                this.count++;
+                return;
+            }
+
+            for (int col = 0; col < this.queens.GetLength(0); col++)
+            {
+                if (this.queens[row, col] == 0)
+                {
+                    MarkQueen(row, col);
+                    this.Recursions(row + 1);
+                    UnMarkQueen(row, col);
+                }
+
+            }
+        }
+
+        private void UnMarkQueen(int row, int col)
+        {
+            for (int i = row; i < this.queens.GetLength(0); i++)
+            {
+                this.queens[i, col] -= 1;
+
+                if (col + (i - row) < this.queens.GetLength(0))
+                {
+                    this.queens[i, col + (i - row)] -= 1;
+                }
+
+                if (col - (i - row) >= 0)
+                {
+                    this.queens[i, col - (i - row)] -= 1;
+                }
+
+            }
+        }
+
+        private void MarkQueen(int row, int col)
+        {
+            for (int i = row; i < this.queens.GetLength(0); i++)
+            {
+                this.queens[i, col] += 1;
+
+                if (col + (i - row) < this.queens.GetLength(0))
+                {
+                    this.queens[i, col + (i - row)] += 1;
+                }
+
+                if (col - (i - row) >= 0)
+                {
+                    this.queens[i, col - (i - row)] += 1;
+                }
+            }
+        }
+    }
+}
---
+++ Data Structures&Аlgorithms/Recursion Homework/12.QueensPuzzle/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _12.QueensPuzzle
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please, enter the size of the board:");
+            int boardSize = int.Parse(Console.ReadLine());
+
+            QueenPuzzleSolver solver = new QueenPuzzleSolver(boardSize);
+            int result = solver.FindAllSolutions();
+            Console.WriteLine($"Solution: {result}");
+        }
+    }
+}
